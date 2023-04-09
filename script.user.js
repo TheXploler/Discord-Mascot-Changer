52be3f0abdbdac35a31294dae1c0b58f24e7d2ab
Fandrest <fandrest@duck.com>
2023-04-09 18:52:43 +0700

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Mascot Changer
+// @namespace    http://101.50.2.18
+// @version      1.0
+// @description  Changes the web Discord nitro mascot to a saucy protogen
+// @author       Fandrest
+// @match        https://discord.com/nitro
+// @grant        none
+// ==/UserScript==
+
+const RemoveVideo = document.querySelector('.video-link-new.w-embed');
+RemoveVideo.innerHTML = '';
+document.getElementsByClassName("hero-bg-wrapper video-new2")[0] .style.backgroundImage="url('https://i.ytimg.com/vi/EQ-xGx6MwVs/maxresdefault.jpg')";
